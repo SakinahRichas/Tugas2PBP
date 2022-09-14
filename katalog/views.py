c218911d272f6799027a1c24f0faabf80cb5d43c
SakinahRichas <sakinahrchs@gmail.com>
2022-09-14 15:34:55 +0700

    Tugas2Sakinah

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+def show_catalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog' : data_barang_katalog,
+        'nama' : 'Sakinah Richas',
+        'studentid' : '2106751511'
+    }
+    return render (request, "katalog.html", context)
