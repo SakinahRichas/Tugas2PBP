c89428eab231177af4fa6422ef9876420952534f
SakinahRichas <sakinahrchs@gmail.com>
2022-09-29 05:12:56 +0700

    tugas4

---
+++ todolist/views.py
@@ -0,0 +1,70 @@
+import imp
+from django.shortcuts import render
+from todolist.models import Task
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
+from todolist.forms import CreateTask
+# Create your views here.
+
+@login_required(login_url='/todolist/login/')
+def show_todolist(request):
+    user = request.user
+    data_todolist = Task.objects.filter(user__pk=user.pk)
+    context = {
+        'username' : user.username,
+        'todolist' : data_todolist,
+        
+    }
+    return render (request, "todolist.html", context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('todolist:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('todolist:show_todolist')
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('todolist:login')
+
+@login_required(login_url='/todolist/login/')
+def create_task(request):
+    #user = request.user
+    form = CreateTask()
+    if request.method == 'POST':
+        form = CreateTask(request.POST, request.FILES)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+        #Task.objects.create(user_id=user.id, title=title, description=description, date=date)
+            return redirect ('todolist:show_todolist')
+        else:
+            form = CreateTask(initial={'user': request.user})
+    context = {'form':form}
+    return render(request, 'createtask.html', context)
